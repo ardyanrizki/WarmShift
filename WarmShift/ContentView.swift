751216b97b89358e78d49c39c536d5a3b71fe780
Ardyan <ardyanrizki@icloud.com>
2025-02-28 10:51:29 +0700

    Initial Commit

---
+++ WarmShift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarmShift
+//
+//  Created by Muhammad Rizki Ardyan on 28/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarmShift/WarmShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmShiftApp.swift
+//  WarmShift
+//
+//  Created by Muhammad Rizki Ardyan on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WarmShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
