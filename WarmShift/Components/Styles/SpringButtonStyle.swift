bff25851476a26691e4e78f7044deed46819cac6
Ardyan <ardyanrizki@icloud.com>
2025-03-01 22:28:53 +0700

    spring button: fix press and scroll functionality conflict

--- WarmShift/Components/Styles/SpringButtonStyle.swift
+++ WarmShift/Components/Styles/SpringButtonStyle.swift
@@ -3,12 +3,33 @@
 import SwiftUI
 
 struct SpringButtonStyle: ButtonStyle {
+    var action: () -> Void
+    
+    @State private var isInvalidateAction: Bool = false
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
             .animation(.bouncy, value: configuration.isPressed)
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 10)
+                    .onChanged({ _ in
+                        isInvalidateAction = true
+                    })
+            )
+            .highPriorityGesture(TapGesture())
+            .onChange(of: configuration.isPressed) { wasPressed, isPressed in
+                Task {
+                    if wasPressed && !isPressed && !isInvalidateAction {
+                        try? await Task.sleep(for: .milliseconds(200))
+                        action()
+                    } else {
+                        isInvalidateAction = false
+                    }
+                }
+            }
             .sensoryFeedback(.impact(flexibility: .solid), trigger: configuration.isPressed) { oldVal, newVal in
-                oldVal && !newVal
+                oldVal && !newVal && !isInvalidateAction
             }
     }
 }
